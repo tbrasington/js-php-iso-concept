6e53ad9356dc192547e1836735ff2165616dfb65
Thomas Brasington <tbrasington@gmail.com>
2016-04-04 13:57:07 +0100

    dynamic json for page render

--- elements.php
+++ elements.php
@@ -15,12 +15,61 @@ class elements {
 
 	}
 	
+	function render($_data,$parent) {
+		
+		foreach ($_data as $attribute) {
+			
+			//print_r($attribute);
+			
+			$element_type = $attribute['type'];
+			
+			if( method_exists($this, $element_type) ) {
+				
+				$node = $this->$element_type($attribute);	
+				$rendered_node = $parent->appendChild($node);
+				
+				if ( is_array($attribute['content'])){
+					// loop
+					$child_content = $this->render($attribute['content'], $rendered_node);
+				} else {
+					// nothing else
+				}
+			}
+
+		}
+	}
+	
+	function block($_data) {
+		
+		$data = $_data;	
+		
+	//	print_r($data);
+  	
+		$node = $this->dom->createElement("div");
+		
+		$node->setAttribute('id', $data['id']);
+		
+		
+		if(is_string($data['content'])) { 
+			$node->appendChild($this->dom->createTextNode($data['content']));
+		}
+		
+		return $node;
+	}
+	
 	function heading($_data) {
 		
 		$data = $_data;	
   	
-		$node = $doc->createElement($element_type);
-		$node->setAttribute('id', $attributes['id']);
+		$node = $this->dom->createElement("h");
+		
+		$node->setAttribute('id', $data['id']);
+		
+		if(is_string($data['content'])) { 
+			$node->appendChild($this->dom->createTextNode($data['content']));
+		}
+
+		return $node;		
 	}
 	
 	function __destruct() {
