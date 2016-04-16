9581ee7ae992aed417c49bdc8c7054cf1d93bda9
Thomas Brasington <tbrasington@gmail.com>
2016-04-16 16:08:04 +0100

    cycling through the gallery

--- app/js/modules/gallery.js
+++ app/js/modules/gallery.js
@@ -19,24 +19,83 @@ function module_events_gallery(root_element){
 	    }
 	}
 	
-	var gallery_next = function() {
-	    console.log('next');
+	var gallery_next = function(gallery) {
+	    
+	    
+        var tmp_gallery_item = gallery.find('.gallery-current')
+        tmp_gallery_item.addClass('gallery-animate-next');
+
+        tmp_gallery_item.bind('transitionend', function(evt) {
+            tmp_gallery_item.removeClass('gallery-animate-next')
+        });
+
+        //remove the peaking class
+        tmp_gallery_item.removeClass('gallery-peaking-next');
+
+        // the next item remove its peaking class
+        var next_position = gallery.data('position') + 1;
+	    if(next_position>=gallery.data('total')) next_position = 0; 
+		
+		var tmp_next_el = gallery.find('.gallery-card:eq('+next_position+")");
+		tmp_next_el.removeClass('gallery-peak-next');
+
+	    
+	    gallery.data('position',next_position)
+	    gallery_set_current(gallery);
+	    
 	}
 	
-	var gallery_previous = function() {
-	    console.log('previous');
+	// prepare to go to the previous element
+	var gallery_previous = function(gallery) {
+	   
+        var tmp_gallery_item = gallery.find('.gallery-current')
+        tmp_gallery_item.addClass('gallery-animate-previous');
+
+        tmp_gallery_item.bind('transitionend', function(evt) {
+            tmp_gallery_item.removeClass('gallery-animate-previous')
+        });
+
+        //remove the peaking class
+        tmp_gallery_item.removeClass('gallery-peaking-previous');
+
+        // the next item remove its peaking class
+        var next_position = gallery.data('position') - 1;
+	    if(next_position<0) next_position = gallery.data('total')-1;  
+		
+		var tmp_next_el = gallery.find('.gallery-card:eq('+next_position+")");
+		tmp_next_el.removeClass('gallery-peak-previous');
+
+	    
+	    gallery.data('position',next_position)
+	    gallery_set_current(gallery);
 	}
 	
+	// set the current element
+	var gallery_set_current = function(gallery) {
+		// card
+		gallery.find('.gallery-current').removeClass('gallery-current');
+		gallery.find('.gallery-card:eq('+gallery.data('position')+")").addClass('gallery-current');
+		// numbers
+		gallery.find('.gallery-number.selected').removeClass('selected');
+		gallery.find('.gallery-number:eq('+gallery.data('position')+")").addClass('selected');
+	}
+	
+	// number event
 	root_element.on('click', '.gallery-number', function(evt){
+
 		var id = $(this).parent().attr('id').replace('gallery-numbers-','');
-		gallery_setup( $('#gallery-'+id), id, gallery_next);
+		
+		$('#gallery-'+id).data('position',$(this).data('position'));
+
+		gallery_setup( $('#gallery-'+id), id, gallery_set_current($('#gallery-'+id)));
+		
 	});
 	
 	
+	// hover events
 	root_element.on('click', '.gallery-right', function(evt){
-		
 		var id = $(this).parent().attr('id').replace('gallery-','');
-		gallery_setup( $('#gallery-'+id), id, gallery_next);
+		gallery_setup( $('#gallery-'+id), id, gallery_next($('#gallery-'+id)));
 		
 	}).on('mouseenter', '.gallery-right', function(evt){
 		
@@ -63,9 +122,9 @@ function module_events_gallery(root_element){
 	});
 	
 	root_element.on('click', '.gallery-left', function(evt){
-		
+
 		var id = $(this).parent().attr('id').replace('gallery-','');
-		gallery_setup( $('#gallery-'+id), id, gallery_previous);
+		gallery_setup( $('#gallery-'+id), id, gallery_previous($('#gallery-'+id)));
 	
 	}).on('mouseenter', '.gallery-left', function(evt){
 		
